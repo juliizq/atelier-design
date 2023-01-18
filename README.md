1cabcc156108906f7ce828ed4390e66031ceeac7
Stagiaire ChargeMi <juliizquierdo15@gmail.com>
2023-01-18 20:25:13 +0100

    refactoring

--- README.md
+++ README.md
@@ -6,10 +6,10 @@ Le projet a été réalisé avec html, sass et javascript.
 
 ## ✨ To do :
 - Ajouter des animations : Scroll Reveal dans chaque section et illustration en section avantages.
-- Fixer scroll dans le menu mobile
-- Améliorer le desing responsive
+- Fixer scroll dans le menu mobile.
+- Améliorer le desing responsive.
 - Améliorez paddings, margins, et responsive design la fonction cal().
-- Améliorez la position de l'illustration avec des cercles dans la section des avantages
+- Améliorez la position de l'illustration avec des cercles dans la section des avantages.
 
 
 
--- main.js
+++ main.js
@@ -1,6 +1,15 @@
-const buttonOpen = document.querySelector('.menu');
+const buttonOpen = document.querySelector('.button-open');
+const buttonClose = document.querySelector('.button-close')
 const menuMobile = document.querySelector('.menu-mobile');
 
 buttonOpen.addEventListener('click', () => {
   menuMobile.classList.toggle('is-open');
+  buttonOpen.style.display = 'none';
+  buttonClose.style.display = 'flex';
+})
+
+buttonClose.addEventListener('click', () => {
+  menuMobile.classList.toggle('is-open');
+  buttonOpen.style.display = 'flex';
+  buttonClose.style.display = 'none';
 })
