a5f968f88c7494334ad8927ed7ed436a4bf73a61
Stagiaire ChargeMi <juliizquierdo15@gmail.com>
2023-01-19 01:28:31 +0100

    add animations ✨

--- main.js
+++ main.js
@@ -1,15 +1,80 @@
-const buttonOpen = document.querySelector('.button-open');
-const buttonClose = document.querySelector('.button-close')
-const menuMobile = document.querySelector('.menu-mobile');
+const buttonOpen = document.querySelector(".button-open");
+const buttonClose = document.querySelector(".button-close");
+const menuMobile = document.querySelector(".menu-mobile");
 
-buttonOpen.addEventListener('click', () => {
-  menuMobile.classList.toggle('is-open');
-  buttonOpen.style.display = 'none';
-  buttonClose.style.display = 'flex';
-})
+buttonOpen.addEventListener("click", () => {
+  menuMobile.classList.toggle("is-open");
+  buttonOpen.style.display = "none";
+  buttonClose.style.display = "flex";
+});
 
-buttonClose.addEventListener('click', () => {
-  menuMobile.classList.toggle('is-open');
-  buttonOpen.style.display = 'flex';
-  buttonClose.style.display = 'none';
-})
+buttonClose.addEventListener("click", () => {
+  menuMobile.classList.toggle("is-open");
+  buttonOpen.style.display = "flex";
+  buttonClose.style.display = "none";
+});
+
+
+const left = {
+  origin: "left",
+};
+const right = {
+  origin: "right",
+};
+const reveal = {
+  duration: 300,
+  scale: 1,
+  distance: "30px",
+  easing: "ease-out",
+  interval: 200,
+};
+
+const revealAdvantages = {
+  duration: 200,
+  distance: "20px",
+  easing: "ease-out",
+};
+
+ScrollReveal().reveal("body", {
+  duration: 500,
+});
+
+//Hero
+
+ScrollReveal().reveal(
+  ".container-image-hero-laptop,.container-image-hero-mobile,.hero-title,.hero-description,.hero-cta,.scroll-down",
+  reveal
+);
+
+ScrollReveal().reveal(
+  ".container-image-hero-laptop,.container-image-hero-mobile",
+  right
+);
+
+ScrollReveal().reveal(
+  ".hero-title,.hero-description,.hero-cta.scroll-down",
+  left
+);
+
+//About
+
+ScrollReveal().reveal(
+  ".container-about-image-mobile,.container-about-image-laptop.content-about,.about-description,.about-cta",
+  reveal
+);
+
+ScrollReveal().reveal(".content-about,.about-description,.about-cta", right);
+
+//Advantages
+
+ScrollReveal().reveal(
+  ".content-title,.slider,.number-slider,.advantages-cta,.illustration-laptop,.illustration-mobile",
+  reveal
+);
+
+ScrollReveal().reveal(".illustration-laptop,.illustration-mobile", right);
+
+ScrollReveal().reveal(
+  ".content-title,.slider,.number-slider,.advantages-cta",
+  left
+);
